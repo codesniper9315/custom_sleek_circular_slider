a0a0a3db74695ba9709fa001eac900ea443363e6
iscojin <flutterdev9715@gmail.com>
2022-04-04 21:47:32 -0400

    test9

--- lib/src/circular_slider.dart
+++ lib/src/circular_slider.dart
@@ -48,8 +48,9 @@ class CustomSleekCircularSlider extends StatefulWidget {
   Offset get endOffset {
     double radius = math.min(width / 2, height / 2) - appearance.progressBarWidth * 0.5;
     Offset center = Offset(width / 2, height / 2);
-    print(endOffset);
-    return degreesToCoordinates(center, -math.pi / 2 + angle, radius);
+    Offset offset = degreesToCoordinates(center, -math.pi / 2 + angle, radius);
+    print(offset);
+    return offset;
   }
 
   const CustomSleekCircularSlider({
